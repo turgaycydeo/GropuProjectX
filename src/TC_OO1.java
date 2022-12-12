5acef703519f5bf0776b11f0c2a8154f46d47f4c
fatmakayhan <fatmakayhan2018@gmail.com>
2022-12-12 19:55:47 +0100

    test 001

--- src/TC_OO1.java
+++ src/TC_OO1.java
@@ -2,8 +2,9 @@ public class TC_OO1 {
 
 
     //whatever, you all so nice!
-    // tc001 started
-    // part 1
+    // tc001 started adfhaıfhıahfafh
+    // part 1 ahjah
+    //fajfıajfaoıjaıfjaıofj
     // part 2
 
 }
